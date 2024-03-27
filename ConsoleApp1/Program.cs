73cad95cbed541672df64cf1feee51b4a550d034
unknown <majzikricsi@outlook.hu>
2024-03-27 11:13:36 +0100

    secon kommentes commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            // git add . hozzáad minden file-t a mappában
+            // git remote add origin "https://github.com/L0neW/ConsoleAPP1.git"
+            // git commit -m  "firs commit" <<--ide az üzenet kell
+            // git branch -M master
+            // git push -u origin master <<--elvileg ez tölti fel a remote repoba
         }
     }
 }
